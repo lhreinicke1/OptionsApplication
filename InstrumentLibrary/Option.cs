b733eb78e4e8fa0c87698624e5082c211dc2e0dc
Luís Henrique Reinicke <lhreinicke@hotmail.com>
2018-01-05 01:02:15 -0200

    Adicionar arquivos de projeto.

---
+++ InstrumentLibrary/Option.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathNet.Numerics.Distributions;
+
+namespace OptionLibrary
+{
+    public class Option
+    {
+        public double Premium { get; protected set; }
+        public double D1 { get; protected set; }
+        public double D2 { get; protected set; }
+
+        public double Spot { get; set; }
+        public double Strike { get; set; }
+        public double Interest { get; set; }
+        public double Volatility { get; set; }
+        public double Time { get; set; }
+
+        public Option():
+            this(0.0, 0.0, 0.0, 0.0, 0.0)
+        {
+        }
+
+        public Option(double assetPrice, double strike, double volatility, 
+            double interest, double time)
+        {
+            Spot = assetPrice;
+            Strike = strike;
+            Volatility = volatility;
+            Interest = interest;
+            Time = time;
+        }
+
+        public void CalculateD1()
+        {
+            if (Time <= 0 || Volatility <= 0 || Strike <= 0 || Spot <= 0)
+            {
+                D1 = 0;
+            }
+            else
+            {
+                D1 = (Math.Log(Spot / Strike) + (Interest + Volatility * Volatility / 2.0) * Time) /
+                    (Volatility * Math.Sqrt(Time));
+            }
+        }
+
+        public void CalculateD2()
+        {
+            if (Time <= 0 || Volatility <= 0 || Strike <= 0 || Spot <= 0)
+            {
+                D2 = 0;
+            }
+            else
+            {
+                D2 = (Math.Log(Spot / Strike) + (Interest - Volatility * Volatility / 2.0) * Time) /
+                    (Volatility * Math.Sqrt(Time));
+            }
+        }
+
+        public void CalculatePremium()
+        {
+            CalculateD1();
+            CalculateD2();
+
+            if (D1 == 0 || D2 == 0)
+            {
+                Premium = Math.Max(Strike - Spot, 0);
+            }
+            else
+            {
+                Premium = Spot * Normal.CDF(0, 1, D1) -
+                        Math.Exp(-Interest * Time) * Strike * Normal.CDF(0, 1, D2);
+            }
+        }
+    }
+}
---
+++ OptionLibraryTests/OptionTests.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OptionLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionLibrary.Tests
+{
+    [TestClass()]
+    public class OptionTests
+    {
+        [TestMethod()]
+        public void CalculateD1Test()
+        {
+            Option option = new Option();
+
+            option.CalculateD1();
+
+            Assert.AreEqual(option.D1, 0);
+            
+            option = new Option(100, 100, 0, 0.01, 1);
+
+            option.CalculateD1();
+
+            Assert.AreEqual(option.D1, 0);
+
+            option = new Option(100, 100, 0.01, 0.01, 0);
+
+            option.CalculateD1();
+
+            Assert.AreEqual(option.D1, 0);
+
+            option = new Option(100, 100, 0, 0.01, 0);
+
+            option.CalculateD1();
+
+            Assert.AreEqual(option.D1, 0);
+
+            option = new Option(100, 0, 0.10, 0.01, 1);
+
+            option.CalculateD1();
+
+            Assert.AreEqual(option.D1, 0);
+
+            option = new Option(100, 100, 0.10, 0.01, 1);
+
+            option.CalculateD1();
+
+            Assert.AreEqual(option.D1, 0.15, 0.01);
+
+            option = new Option(100, 110, 0.10, 0.01, 1);
+
+            option.CalculateD1();
+
+            Assert.AreEqual(option.D1, -0.8, 0.05);
+
+            option = new Option(100, 90, 0.10, 0.01, 1);
+
+            option.CalculateD1();
+
+            Assert.AreEqual(option.D1, 1.2, 0.02);
+        }
+
+        [TestMethod()]
+        public void CalculateD2Test()
+        {
+            Option option = new Option();
+
+            option.CalculateD2();
+
+            Assert.AreEqual(option.D2, 0);
+
+            option = new Option(40, 40, 0, 0.1, 1);
+
+            option.CalculateD2();
+
+            Assert.AreEqual(option.D2, 0);
+
+            option = new Option(40, 40, 0.01, 0.1, 0);
+
+            option.CalculateD2();
+
+            Assert.AreEqual(option.D2, 0);
+
+            option = new Option(40, 40, 0, 0.1, 0);
+
+            option.CalculateD2();
+
+            Assert.AreEqual(option.D2, 0);
+
+            option = new Option(40, -10, 0.10, 0.1, 1);
+
+            option.CalculateD2();
+
+            Assert.AreEqual(option.D2, 0);
+
+            option = new Option(40, 40, 0.10, 0.1, 1);
+
+            option.CalculateD2();
+
+            Assert.AreEqual(option.D2, 0.95);
+
+            option = new Option(40, 45, 0.10, 0.1, 1);
+
+            option.CalculateD2();
+
+            Assert.AreEqual(option.D2, -0.2, 0.05);
+
+            option = new Option(40, 35, 0.10, 0.1, 1);
+
+            option.CalculateD2();
+
+            Assert.AreEqual(option.D2, 2.3, 0.02);
+        }
+
+        [TestMethod()]
+        public void CalculatePremiumTest()
+        {
+            Option option = new Option(10, 10, 0.1, 0.1, 0);
+
+            option.CalculatePremium();
+
+            Assert.AreEqual(option.Premium, 0);
+
+            option = new Option(10, 15, 0.1, 0.1, 0);
+
+            option.CalculatePremium();
+
+            Assert.AreEqual(option.Premium, 5);
+
+            option = new Option(80, 75, 0.1, 0.1, 0);
+
+            option.CalculatePremium();
+
+            Assert.AreEqual(option.Premium, 0);
+
+            option = new Option(80, 90, 0.1, 0.1, 0.5);
+
+            option.CalculatePremium();
+
+            Assert.AreEqual(option.Premium, 0.5, 0.03);
+
+            option = new Option(80, 150, 0.1, 0.1, 0.5);
+
+            option.CalculatePremium();
+
+            Assert.AreEqual(option.Premium, 0, 0.0001);
+
+            option = new Option(80, 1, 0.1, 0.1, 0.5);
+
+            option.CalculatePremium();
+
+            Assert.AreEqual(option.Premium, 79, 0.05);
+
+        }
+    }
+}
